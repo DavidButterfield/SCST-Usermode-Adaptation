DavidButterfield/SCST-Usermode-Adaptation

commit c24eec41ceb320444dea5f5ff78e5608ce85cd14

Assigning CPU affinity to threads and connections
git-svn-id: https://svn.code.sf.net/p/scst/svn/trunk@2317 d57e44dd-8a1f-0410-8b47-8ef2f437770f

iscsi-scst/kernel/iscsi.c
@@ -47,23 +47,13 @@ unsigned long iscsi_trace_flag = ISCSI_DEFAULT_LOG_FLAGS;
 
 static struct kmem_cache *iscsi_cmnd_cache;
 
-DEFINE_SPINLOCK(iscsi_rd_lock);
-LIST_HEAD(iscsi_rd_list);
-DECLARE_WAIT_QUEUE_HEAD(iscsi_rd_waitQ);
+static DEFINE_MUTEX(iscsi_threads_pool_mutex);
+static LIST_HEAD(iscsi_thread_pools_list);
 
-DEFINE_SPINLOCK(iscsi_wr_lock);
-LIST_HEAD(iscsi_wr_list);
-DECLARE_WAIT_QUEUE_HEAD(iscsi_wr_waitQ);
+struct iscsi_thread_pool *iscsi_main_thread_pool;
 
 static struct page *dummy_page;
 static struct scatterlist dummy_sg;
-
-struct iscsi_thread_t {
-	struct task_struct *thr;
-	struct list_head threads_list_entry;
-};
-
-static LIST_HEAD(iscsi_threads_list);
 
 static void cmnd_remove_data_wait_hash(struct iscsi_cmnd *cmnd);
 static void iscsi_send_task_mgmt_resp(struct iscsi_cmnd *req, int status);
@@ -2241,7 +2231,7 @@ static void __cmnd_abort(struct iscsi_cmnd *cmnd)
 	 * Lock to sync with iscsi_check_tm_data_wait_timeouts(), including
 	 * CMD_ABORTED bit set.
 	 */
-	spin_lock_bh(&iscsi_rd_lock);
+	spin_lock_bh(&conn->conn_thr_pool->rd_lock);
 
 	/*
 	 * We suppose that preliminary commands completion is tested by
@@ -2255,7 +2245,7 @@ static void __cmnd_abort(struct iscsi_cmnd *cmnd)
 	TRACE_MGMT_DBG("Setting conn_tm_active for conn %p", conn);
 	conn->conn_tm_active = 1;
 
-	spin_unlock_bh(&iscsi_rd_lock);
+	spin_unlock_bh(&conn->conn_thr_pool->rd_lock);
 
 	/*
 	 * We need the lock to sync with req_add_to_write_timeout_list() and
@@ -3197,11 +3187,12 @@ static void iscsi_preprocessing_done(struct scst_cmd *scst_cmd)
 static void iscsi_try_local_processing(struct iscsi_cmnd *req)
 {
 	struct iscsi_conn *conn = req->conn;
+	struct iscsi_thread_pool *p = conn->conn_thr_pool;
 	bool local;
 
 	TRACE_ENTRY();
 
-	spin_lock_bh(&iscsi_wr_lock);
+	spin_lock_bh(&p->wr_lock);
 	switch (conn->wr_state) {
 	case ISCSI_CONN_WR_STATE_IN_LIST:
 		list_del(&conn->wr_list_entry);
@@ -3218,7 +3209,7 @@ static void iscsi_try_local_processing(struct iscsi_cmnd *req)
 		local = false;
 		break;
 	}
-	spin_unlock_bh(&iscsi_wr_lock);
+	spin_unlock_bh(&p->wr_lock);
 
 	if (local) {
 		int rc = 1;
@@ -3229,7 +3220,7 @@ static void iscsi_try_local_processing(struct iscsi_cmnd *req)
 				break;
 		} while (req->not_processed_rsp_cnt != 0);
 
-		spin_lock_bh(&iscsi_wr_lock);
+		spin_lock_bh(&p->wr_lock);
 #ifdef CONFIG_SCST_EXTRACHECKS
 		conn->wr_task = NULL;
 #endif
@@ -3238,12 +3229,12 @@ static void iscsi_try_local_processing(struct iscsi_cmnd *req)
 				"(conn %p)", conn);
 			conn->wr_state = ISCSI_CONN_WR_STATE_SPACE_WAIT;
 		} else if (test_write_ready(conn)) {
-			list_add_tail(&conn->wr_list_entry, &iscsi_wr_list);
+			list_add_tail(&conn->wr_list_entry, &p->wr_list);
 			conn->wr_state = ISCSI_CONN_WR_STATE_IN_LIST;
-			wake_up(&iscsi_wr_waitQ);
+			wake_up(&p->wr_waitQ);
 		} else
 			conn->wr_state = ISCSI_CONN_WR_STATE_IDLE;
-		spin_unlock_bh(&iscsi_wr_lock);
+		spin_unlock_bh(&p->wr_lock);
 	}
 
 	TRACE_EXIT();
@@ -3646,6 +3637,27 @@ out_err:
 	goto out;
 }
 
+static int iscsi_cpu_mask_changed_aen(struct scst_aen *aen)
+{
+	int res = SCST_AEN_RES_SUCCESS;
+	struct scst_session *scst_sess = scst_aen_get_sess(aen);
+	struct iscsi_session *sess = scst_sess_get_tgt_priv(scst_sess);
+
+	TRACE_ENTRY();
+
+	TRACE_MGMT_DBG("CPU mask changed AEN to sess %p (initiator %s)", sess,
+		sess->initiator_name);
+
+	mutex_lock(&sess->target->target_mutex);
+	iscsi_sess_force_close(sess);
+	mutex_unlock(&sess->target->target_mutex);
+
+	scst_aen_done(aen);
+
+	TRACE_EXIT_RES(res);
+	return res;
+}
+
 static int iscsi_report_aen(struct scst_aen *aen)
 {
 	int res;
@@ -3656,6 +3668,9 @@ static int iscsi_report_aen(struct scst_aen *aen)
 	switch (event_fn) {
 	case SCST_AEN_SCSI:
 		res = iscsi_scsi_aen(aen);
+		break;
+	case SCST_AEN_CPU_MASK_CHANGED:
+		res = iscsi_cpu_mask_changed_aen(aen);
 		break;
 	default:
 		TRACE_MGMT_DBG("Unsupported AEN %d", event_fn);
@@ -3835,52 +3850,179 @@ struct scst_tgt_template iscsi_template = {
 	.get_scsi_transport_version = iscsi_get_scsi_transport_version,
 };
 
-static __init int iscsi_run_threads(int count, char *name, int (*fn)(void *))
+int iscsi_threads_pool_get(const struct cpumask *cpu_mask,
+	struct iscsi_thread_pool **out_pool)
 {
-	int res = 0;
-	int i;
-	struct iscsi_thread_t *thr;
+	int res;
+	struct iscsi_thread_pool *p;
+	struct iscsi_thread *t, *tt;
+	int i, j, count;
 
-	for (i = 0; i < count; i++) {
-		thr = kmalloc(sizeof(*thr), GFP_KERNEL);
-		if (!thr) {
-			res = -ENOMEM;
-			PRINT_ERROR("Failed to allocate thr %d", res);
-			goto out;
+	TRACE_ENTRY();
+
+	mutex_lock(&iscsi_threads_pool_mutex);
+
+	list_for_each_entry(p, &iscsi_thread_pools_list,
+			thread_pools_list_entry) {
+		if ((cpu_mask == NULL) ||
+		    __cpus_equal(cpu_mask, &p->cpu_mask, nr_cpumask_bits)) {
+			p->thread_pool_ref++;
+			TRACE_DBG("iSCSI thread pool %p found (new ref %d)",
+				p, p->thread_pool_ref);
+			res = 0;
+			goto out_unlock;
 		}
-		thr->thr = kthread_run(fn, NULL, "%s%d", name, i);
-		if (IS_ERR(thr->thr)) {
-			res = PTR_ERR(thr->thr);
-			PRINT_ERROR("kthread_create() failed: %d", res);
-			kfree(thr);
-			goto out;
-		}
-		list_add_tail(&thr->threads_list_entry, &iscsi_threads_list);
 	}
 
-out:
+	TRACE_DBG("%s", "Creating new iSCSI thread pool");
+
+	p = kzalloc(sizeof(*p), GFP_KERNEL);
+	if (p == NULL) {
+		PRINT_ERROR("Unable to allocate iSCSI thread pool (size %zd)",
+			sizeof(*p));
+		res = -ENOMEM;
+		if (!list_empty(&iscsi_thread_pools_list)) {
+			PRINT_WARNING("%s", "Using global iSCSI thread pool "
+				"instead");
+			p = list_entry(iscsi_thread_pools_list.next,
+				struct iscsi_thread_pool,
+				thread_pools_list_entry);
+		} else
+			res = -ENOMEM;
+		goto out_unlock;
+	}
+
+	spin_lock_init(&p->rd_lock);
+	INIT_LIST_HEAD(&p->rd_list);
+	init_waitqueue_head(&p->rd_waitQ);
+	spin_lock_init(&p->wr_lock);
+	INIT_LIST_HEAD(&p->wr_list);
+	init_waitqueue_head(&p->wr_waitQ);
+	if (cpu_mask == NULL)
+		cpus_setall(p->cpu_mask);
+	else {
+		cpus_clear(p->cpu_mask);
+		for_each_cpu(i, cpu_mask)
+			cpu_set(i, p->cpu_mask);
+	}
+	p->thread_pool_ref = 1;
+	INIT_LIST_HEAD(&p->threads_list);
+
+	if (cpu_mask == NULL)
+		count = max((int)num_online_cpus(), 2);
+	else {
+		count = 0;
+		for_each_cpu(i, cpu_mask)
+			count++;
+	}
+
+	for (j = 0; j < 2; j++) {
+		int (*fn)(void *);
+		char name[25];
+		static int major;
+
+		if (j == 0)
+			fn = istrd;
+		else
+			fn = istwr;
+
+		major++;
+
+		for (i = 0; i < count; i++) {
+			if (j == 0) {
+				if (cpu_mask == NULL)
+					snprintf(name, sizeof(name), "iscsird%d", i);
+				else
+					snprintf(name, sizeof(name), "iscsird%d_%d",
+						major, i);
+			} else {
+				if (cpu_mask == NULL)
+					snprintf(name, sizeof(name), "iscsiwr%d", i);
+				else
+					snprintf(name, sizeof(name), "iscsiwr%d_%d",
+						major, i);
+			}
+
+			t = kmalloc(sizeof(*t), GFP_KERNEL);
+			if (t == NULL) {
+				res = -ENOMEM;
+				PRINT_ERROR("Failed to allocate thread %s "
+					"(size %zd)", name, sizeof(*t));
+				goto out_free;
+			}
+
+			t->thr = kthread_run(fn, p, name);
+			if (IS_ERR(t->thr)) {
+				res = PTR_ERR(t->thr);
+				PRINT_ERROR("kthread_run() for thread %s failed: %d",
+					name, res);
+				kfree(t);
+				goto out_free;
+			}
+			list_add_tail(&t->threads_list_entry, &p->threads_list);
+		}
+	}
+
+	list_add_tail(&p->thread_pools_list_entry, &iscsi_thread_pools_list);
+	res = 0;
+
+	TRACE_DBG("Created iSCSI thread pool %p", p);
+
+out_unlock:
+	mutex_unlock(&iscsi_threads_pool_mutex);
+
+	if (out_pool != NULL)
+		*out_pool = p;
+
+	TRACE_EXIT_RES(res);
 	return res;
-}
 
-static void iscsi_stop_threads(void)
-{
-	struct iscsi_thread_t *t, *tmp;
-
-	list_for_each_entry_safe(t, tmp, &iscsi_threads_list,
-				threads_list_entry) {
-		int rc = kthread_stop(t->thr);
-		if (rc < 0)
-			TRACE_MGMT_DBG("kthread_stop() failed: %d", rc);
+out_free:
+	list_for_each_entry_safe(t, tt, &p->threads_list, threads_list_entry) {
+		kthread_stop(t->thr);
 		list_del(&t->threads_list_entry);
 		kfree(t);
 	}
+	goto out_unlock;
+}
+
+void iscsi_threads_pool_put(struct iscsi_thread_pool *p)
+{
+	struct iscsi_thread *t, *tt;
+
+	TRACE_ENTRY();
+
+	mutex_lock(&iscsi_threads_pool_mutex);
+
+	p->thread_pool_ref--;
+	if (p->thread_pool_ref > 0) {
+		TRACE_DBG("iSCSI thread pool %p still has %d references)",
+			p, p->thread_pool_ref);
+		goto out_unlock;
+	}
+
+	TRACE_DBG("Freeing iSCSI thread pool %p", p);
+
+	list_for_each_entry_safe(t, tt, &p->threads_list, threads_list_entry) {
+		kthread_stop(t->thr);
+		list_del(&t->threads_list_entry);
+		kfree(t);
+	}
+
+	list_del(&p->thread_pools_list_entry);
+
+	kfree(p);
+
+out_unlock:
+	mutex_unlock(&iscsi_threads_pool_mutex);
+
+	TRACE_EXIT();
 	return;
 }
 
 static int __init iscsi_init(void)
 {
 	int err = 0;
-	int num;
 
 	PRINT_INFO("iSCSI SCST Target - version %s", ISCSI_VERSION_STRING);
 
@@ -3939,13 +4081,7 @@ static int __init iscsi_init(void)
 	iscsi_conn_ktype.sysfs_ops = scst_sysfs_get_sysfs_ops();
 #endif
 
-	num = max((int)num_online_cpus(), 2);
-
-	err = iscsi_run_threads(num, "iscsird", istrd);
-	if (err != 0)
-		goto out_thr;
-
-	err = iscsi_run_threads(num, "iscsiwr", istwr);
+	err = iscsi_threads_pool_get(NULL, &iscsi_main_thread_pool);
 	if (err != 0)
 		goto out_thr;
 
@@ -3956,7 +4092,6 @@ out_thr:
 #ifdef CONFIG_SCST_PROC
 	iscsi_procfs_exit();
 #endif
-	iscsi_stop_threads();
 
 #ifdef CONFIG_SCST_PROC
 out_reg_tmpl:
@@ -3984,7 +4119,9 @@ out_free_dummy:
 
 static void __exit iscsi_exit(void)
 {
-	iscsi_stop_threads();
+	iscsi_threads_pool_put(iscsi_main_thread_pool);
+
+	sBUG_ON(!list_empty(&iscsi_thread_pools_list));
 
 	unregister_chrdev(ctr_major, ctr_name);
 

iscsi-scst/kernel/iscsi.h
@@ -64,10 +64,29 @@ struct iscsi_tgt_params {
 	unsigned int nop_in_interval;
 };
 
-struct network_thread_info {
-	struct task_struct *task;
-	unsigned int ready;
+struct iscsi_thread {
+	struct task_struct *thr;
+	struct list_head threads_list_entry;
 };
+
+struct iscsi_thread_pool {
+	spinlock_t rd_lock;
+	struct list_head rd_list;
+	wait_queue_head_t rd_waitQ;
+
+	spinlock_t wr_lock;
+	struct list_head wr_list;
+	wait_queue_head_t wr_waitQ;
+
+	struct cpumask cpu_mask;
+
+	int thread_pool_ref;
+
+	struct list_head threads_list;
+
+	struct list_head thread_pools_list_entry;
+};
+
 
 struct iscsi_target;
 struct iscsi_cmnd;
@@ -151,6 +170,8 @@ struct iscsi_session {
 	unsigned int sess_reinstating:1;
 	unsigned int sess_shutting_down:1;
 
+	struct iscsi_thread_pool *sess_thr_pool;
+
 	/* All don't need any protection */
 	char *initiator_name;
 	u64 sid;
@@ -196,7 +217,7 @@ struct iscsi_conn {
 	unsigned int rsp_timeout; /* in jiffies */
 
 	/*
-	 * All 2 protected by iscsi_wr_lock. Modified independently to the
+	 * All 2 protected by wr_lock. Modified independently to the
 	 * above field, hence the alignment.
 	 */
 	unsigned short wr_state __attribute__((aligned(sizeof(long))));
@@ -232,7 +253,9 @@ struct iscsi_conn {
 	int hdigest_type;
 	int ddigest_type;
 
-	/* All 6 protected by iscsi_rd_lock */
+	struct iscsi_thread_pool *conn_thr_pool;
+
+	/* All 6 protected by rd_lock */
 	unsigned short rd_state;
 	unsigned short rd_data_ready:1;
 	/* Let's save some cache footprint by putting them here */
@@ -485,14 +508,6 @@ extern struct mutex target_mgmt_mutex;
 extern int ctr_open_state;
 extern const struct file_operations ctr_fops;
 
-extern spinlock_t iscsi_rd_lock;
-extern struct list_head iscsi_rd_list;
-extern wait_queue_head_t iscsi_rd_waitQ;
-
-extern spinlock_t iscsi_wr_lock;
-extern struct list_head iscsi_wr_list;
-extern wait_queue_head_t iscsi_wr_waitQ;
-
 /* iscsi.c */
 extern struct iscsi_cmnd *cmnd_alloc(struct iscsi_conn *,
 	struct iscsi_cmnd *parent);
@@ -512,6 +527,9 @@ extern int iscsi_preliminary_complete(struct iscsi_cmnd *req,
 	struct iscsi_cmnd *orig_req, bool get_data);
 extern int set_scst_preliminary_status_rsp(struct iscsi_cmnd *req,
 	bool get_data, int key, int asc, int ascq);
+extern int iscsi_threads_pool_get(const struct cpumask *cpu_mask,
+	struct iscsi_thread_pool **out_pool);
+extern void iscsi_threads_pool_put(struct iscsi_thread_pool *p);
 
 /* conn.c */
 #ifndef CONFIG_SCST_PROC
@@ -591,6 +609,7 @@ extern int __add_session(struct iscsi_target *,
 	struct iscsi_kern_session_info *);
 extern int __del_session(struct iscsi_target *, u64);
 extern int session_free(struct iscsi_session *session, bool del);
+extern void iscsi_sess_force_close(struct iscsi_session *sess);
 
 /* params.c */
 extern const char *iscsi_get_digest_name(int val, char *res);

scst/include/scst.h
@@ -335,6 +335,11 @@ enum scst_exec_context {
  */
 #define SCST_AEN_SCSI                0
 
+/*
+ * Notifies that CPU affinity mask on the corresponding session changed
+ */
+#define SCST_AEN_CPU_MASK_CHANGED    1
+
 /*************************************************************
  ** Allowed return/status codes for report_aen() callback and
  ** scst_set_aen_delivery_status() function
@@ -2390,6 +2395,9 @@ struct scst_acg {
 	/* Type of I/O initiators groupping */
 	int acg_io_grouping_type;
 
+	/* CPU affinity for threads in this ACG */
+	struct cpumask acg_cpu_mask;
+
 	unsigned int tgt_acg:1;
 
 	/* sysfs release completion */
@@ -3887,6 +3895,7 @@ struct scst_sysfs_work_item {
 				bool is_tgt_kobj;
 				int io_grouping_type;
 				bool enable;
+				struct cpumask cpu_mask;
 			};
 		};
 		struct {

scst/src/scst_main.c
@@ -158,6 +158,8 @@ static int suspend_count;
 
 static int scst_virt_dev_last_id; /* protected by scst_mutex */
 
+struct cpumask default_cpu_mask;
+
 static unsigned int scst_max_cmd_mem;
 unsigned int scst_max_dev_cmd_mem;
 
@@ -1588,9 +1590,15 @@ int scst_add_threads(struct scst_cmd_threads *cmd_threads,
 				cmd_threads, "%s%d", nm, n++);
 		} else if (tgt_dev != NULL) {
 			char nm[11]; /* to limit the name's len */
+			int rc;
 			strlcpy(nm, tgt_dev->dev->virt_name, ARRAY_SIZE(nm));
 			thr->cmd_thread = kthread_create(scst_cmd_thread,
 				cmd_threads, "%s%d_%d", nm, tgt_dev_num, n++);
+			rc = set_cpus_allowed_ptr(thr->cmd_thread,
+				&tgt_dev->sess->acg->acg_cpu_mask);
+			if (rc != 0)
+				PRINT_ERROR("Setting CPU affinity failed: "
+					"%d", rc);
 		} else
 			thr->cmd_thread = kthread_create(scst_cmd_thread,
 				cmd_threads, "scstd%d", n++);
@@ -2182,6 +2190,7 @@ static int __init init_scst(void)
 	init_waitqueue_head(&scst_dev_cmd_waitQ);
 	mutex_init(&scst_suspend_mutex);
 	INIT_LIST_HEAD(&scst_cmd_threads_list);
+	cpus_setall(default_cpu_mask);
 
 	scst_init_threads(&scst_main_cmd_threads);
 

scst/src/scst_priv.h
@@ -193,6 +193,8 @@ extern spinlock_t scst_mgmt_lock;
 extern struct list_head scst_sess_init_list;
 extern struct list_head scst_sess_shut_list;
 
+extern struct cpumask default_cpu_mask;
+
 struct scst_cmd_thread_t {
 	struct task_struct *cmd_thread;
 	struct list_head thread_list_entry;
@@ -549,6 +551,10 @@ void scst_process_reset(struct scst_device *dev,
 
 bool scst_is_ua_global(const uint8_t *sense, int len);
 void scst_requeue_ua(struct scst_cmd *cmd);
+
+struct scst_aen *scst_alloc_aen(struct scst_session *sess,
+	uint64_t unpacked_lun);
+void scst_free_aen(struct scst_aen *aen);
 
 void scst_gen_aen_or_ua(struct scst_tgt_dev *tgt_dev,
 	int key, int asc, int ascq);

scst/src/scst_sysfs.c
@@ -123,6 +123,12 @@ static ssize_t scst_tgt_io_grouping_type_show(struct kobject *kobj,
 static ssize_t scst_tgt_io_grouping_type_store(struct kobject *kobj,
 				    struct kobj_attribute *attr,
 				    const char *buf, size_t count);
+static ssize_t scst_tgt_cpu_mask_show(struct kobject *kobj,
+				   struct kobj_attribute *attr,
+				   char *buf);
+static ssize_t scst_tgt_cpu_mask_store(struct kobject *kobj,
+				    struct kobj_attribute *attr,
+				    const char *buf, size_t count);
 static ssize_t scst_ini_group_mgmt_show(struct kobject *kobj,
 				   struct kobj_attribute *attr,
 				   char *buf);
@@ -154,6 +160,12 @@ static ssize_t scst_acg_io_grouping_type_show(struct kobject *kobj,
 				   struct kobj_attribute *attr,
 				   char *buf);
 static ssize_t scst_acg_io_grouping_type_store(struct kobject *kobj,
+				    struct kobj_attribute *attr,
+				    const char *buf, size_t count);
+static ssize_t scst_acg_cpu_mask_show(struct kobject *kobj,
+				   struct kobj_attribute *attr,
+				   char *buf);
+static ssize_t scst_acg_cpu_mask_store(struct kobject *kobj,
 				    struct kobj_attribute *attr,
 				    const char *buf, size_t count);
 static ssize_t scst_acn_file_show(struct kobject *kobj,
@@ -964,6 +976,11 @@ static struct kobj_attribute scst_tgt_io_grouping_type =
 	       scst_tgt_io_grouping_type_show,
 	       scst_tgt_io_grouping_type_store);
 
+static struct kobj_attribute scst_tgt_cpu_mask =
+	__ATTR(cpu_mask, S_IRUGO | S_IWUSR,
+	       scst_tgt_cpu_mask_show,
+	       scst_tgt_cpu_mask_store);
+
 static struct kobj_attribute scst_rel_tgt_id =
 	__ATTR(rel_tgt_id, S_IRUGO | S_IWUSR, scst_rel_tgt_id_show,
 	       scst_rel_tgt_id_store);
@@ -976,6 +993,11 @@ static struct kobj_attribute scst_acg_io_grouping_type =
 	__ATTR(io_grouping_type, S_IRUGO | S_IWUSR,
 	       scst_acg_io_grouping_type_show,
 	       scst_acg_io_grouping_type_store);
+
+static struct kobj_attribute scst_acg_cpu_mask =
+	__ATTR(cpu_mask, S_IRUGO | S_IWUSR,
+	       scst_acg_cpu_mask_show,
+	       scst_acg_cpu_mask_store);
 
 static ssize_t scst_tgt_enable_show(struct kobject *kobj,
 	struct kobj_attribute *attr, char *buf)
@@ -1180,6 +1202,13 @@ int scst_tgt_sysfs_create(struct scst_tgt *tgt)
 	if (res != 0) {
 		PRINT_ERROR("Can't add attribute %s for tgt %s",
 			scst_tgt_io_grouping_type.attr.name, tgt->tgt_name);
+		goto out_err;
+	}
+
+	res = sysfs_create_file(&tgt->tgt_kobj, &scst_tgt_cpu_mask.attr);
+	if (res != 0) {
+		PRINT_ERROR("Can't add attribute %s for tgt %s",
+			scst_tgt_cpu_mask.attr.name, tgt->tgt_name);
 		goto out_err;
 	}
 
@@ -3143,6 +3172,168 @@ out:
 	return res;
 }
 
+static ssize_t __scst_acg_cpu_mask_show(struct scst_acg *acg, char *buf)
+{
+	int res;
+
+	res = cpumask_scnprintf(buf, SCST_SYSFS_BLOCK_SIZE,
+		&acg->acg_cpu_mask);
+	if (!cpus_equal(acg->acg_cpu_mask, default_cpu_mask))
+		res += sprintf(&buf[res], "\n%s\n", SCST_SYSFS_KEY_MARK);
+
+	return res;
+}
+
+static int __scst_acg_process_cpu_mask_store(struct scst_tgt *tgt,
+	struct scst_acg *acg, struct cpumask *cpu_mask)
+{
+	int res = 0;
+	struct scst_session *sess;
+
+	TRACE_DBG("tgt %p, acg %p", tgt, acg);
+
+	if (mutex_lock_interruptible(&scst_mutex) != 0) {
+		res = -EINTR;
+		goto out;
+	}
+
+	/* Check if tgt and acg not already freed while we were coming here */
+	if (scst_check_tgt_acg_ptrs(tgt, acg) != 0)
+		goto out_unlock;
+
+	cpumask_copy(&acg->acg_cpu_mask, cpu_mask);
+
+	list_for_each_entry(sess, &acg->acg_sess_list, acg_sess_list_entry) {
+		int i;
+		for (i = 0; i < SESS_TGT_DEV_LIST_HASH_SIZE; i++) {
+			struct scst_tgt_dev *tgt_dev;
+			struct list_head *head = &sess->sess_tgt_dev_list[i];
+			list_for_each_entry(tgt_dev, head,
+						sess_tgt_dev_list_entry) {
+				struct scst_cmd_thread_t *thr;
+				if (tgt_dev->active_cmd_threads != &tgt_dev->tgt_dev_cmd_threads)
+					continue;
+				list_for_each_entry(thr,
+						&tgt_dev->active_cmd_threads->threads_list,
+						thread_list_entry) {
+					int rc;
+					rc = set_cpus_allowed_ptr(thr->cmd_thread, cpu_mask);
+					if (rc != 0)
+						PRINT_ERROR("Setting CPU "
+							"affinity failed: %d", rc);
+				}
+			}
+		}
+		if (tgt->tgtt->report_aen != NULL) {
+			struct scst_aen *aen;
+			int rc;
+
+			aen = scst_alloc_aen(sess, 0);
+			if (aen == NULL) {
+				PRINT_ERROR("Unable to notify target driver %s "
+					"about cpu_mask change", tgt->tgt_name);
+				continue;
+			}
+
+			aen->event_fn = SCST_AEN_CPU_MASK_CHANGED;
+
+			TRACE_DBG("Calling target's %s report_aen(%p)",
+				tgt->tgtt->name, aen);
+			rc = tgt->tgtt->report_aen(aen);
+			TRACE_DBG("Target's %s report_aen(%p) returned %d",
+				tgt->tgtt->name, aen, rc);
+			if (rc != SCST_AEN_RES_SUCCESS)
+				scst_free_aen(aen);
+		}
+	}
+
+
+out_unlock:
+	mutex_unlock(&scst_mutex);
+
+out:
+	return res;
+}
+
+static int __scst_acg_cpu_mask_store_work_fn(struct scst_sysfs_work_item *work)
+{
+	return __scst_acg_process_cpu_mask_store(work->tgt, work->acg,
+			&work->cpu_mask);
+}
+
+static ssize_t __scst_acg_cpu_mask_store(struct scst_acg *acg,
+	const char *buf, size_t count)
+{
+	int res;
+	struct scst_sysfs_work_item *work;
+
+	/* cpumask might be too big for stack */
+
+	res = scst_alloc_sysfs_work(__scst_acg_cpu_mask_store_work_fn,
+					false, &work);
+	if (res != 0)
+		goto out;
+
+	/*
+	 * We can't use cpumask_parse_user() here, because it expects
+	 * buffer in the user space.
+	 */
+	res = __bitmap_parse(buf, count, 0, cpumask_bits(&work->cpu_mask),
+				nr_cpumask_bits);
+	if (res != 0) {
+		PRINT_ERROR("__bitmap_parse() failed: %d", res);
+		goto out_release;
+	}
+
+	if (cpus_equal(acg->acg_cpu_mask, work->cpu_mask))
+		goto out;
+
+	work->tgt = acg->tgt;
+	work->acg = acg;
+
+	res = scst_sysfs_queue_wait_work(work);
+
+out:
+	return res;
+
+out_release:
+	scst_sysfs_work_release(&work->sysfs_work_kref);
+	goto out;
+}
+
+static ssize_t scst_tgt_cpu_mask_show(struct kobject *kobj,
+	struct kobj_attribute *attr, char *buf)
+{
+	struct scst_acg *acg;
+	struct scst_tgt *tgt;
+
+	tgt = container_of(kobj, struct scst_tgt, tgt_kobj);
+	acg = tgt->default_acg;
+
+	return __scst_acg_cpu_mask_show(acg, buf);
+}
+
+static ssize_t scst_tgt_cpu_mask_store(struct kobject *kobj,
+	struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int res;
+	struct scst_acg *acg;
+	struct scst_tgt *tgt;
+
+	tgt = container_of(kobj, struct scst_tgt, tgt_kobj);
+	acg = tgt->default_acg;
+
+	res = __scst_acg_cpu_mask_store(acg, buf, count);
+	if (res != 0)
+		goto out;
+
+	res = count;
+
+out:
+	TRACE_EXIT_RES(res);
+	return res;
+}
+
 /*
  * Called with scst_mutex held.
  *
@@ -3241,6 +3432,13 @@ int scst_acg_sysfs_create(struct scst_tgt *tgt,
 		goto out_del;
 	}
 
+	res = sysfs_create_file(&acg->acg_kobj, &scst_acg_cpu_mask.attr);
+	if (res != 0) {
+		PRINT_ERROR("Can't add tgt attr %s for tgt %s",
+			scst_acg_cpu_mask.attr.name, tgt->tgt_name);
+		goto out_del;
+	}
+
 out:
 	TRACE_EXIT_RES(res);
 	return res;
@@ -3293,6 +3491,35 @@ static ssize_t scst_acg_io_grouping_type_store(struct kobject *kobj,
 	acg = container_of(kobj, struct scst_acg, acg_kobj);
 
 	res = __scst_acg_io_grouping_type_store(acg, buf, count);
+	if (res != 0)
+		goto out;
+
+	res = count;
+
+out:
+	TRACE_EXIT_RES(res);
+	return res;
+}
+
+static ssize_t scst_acg_cpu_mask_show(struct kobject *kobj,
+	struct kobj_attribute *attr, char *buf)
+{
+	struct scst_acg *acg;
+
+	acg = container_of(kobj, struct scst_acg, acg_kobj);
+
+	return __scst_acg_cpu_mask_show(acg, buf);
+}
+
+static ssize_t scst_acg_cpu_mask_store(struct kobject *kobj,
+	struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int res;
+	struct scst_acg *acg;
+
+	acg = container_of(kobj, struct scst_acg, acg_kobj);
+
+	res = __scst_acg_cpu_mask_store(acg, buf, count);
 	if (res != 0)
 		goto out;
 
@@ -5199,7 +5426,7 @@ int __init scst_sysfs_init(void)
 		NULL, "scst_uid");
 	if (IS_ERR(sysfs_work_thread)) {
 		res = PTR_ERR(sysfs_work_thread);
-		PRINT_ERROR("kthread_create() for user interface thread "
+		PRINT_ERROR("kthread_run() for user interface thread "
 			"failed: %d", res);
 		sysfs_work_thread = NULL;
 		goto out;

commit 8069718e76049f2140f52cc83bd4f83bafa2bca2

Slightly modified patch from Bart Van Assche <bvanassche@acm.org> fixing build breakage on not too recent kernels introduced by r2317
git-svn-id: https://svn.code.sf.net/p/scst/svn/trunk@2324 d57e44dd-8a1f-0410-8b47-8ef2f437770f

iscsi-scst/kernel/iscsi.c
@@ -3850,7 +3850,7 @@ struct scst_tgt_template iscsi_template = {
 	.get_scsi_transport_version = iscsi_get_scsi_transport_version,
 };
 
-int iscsi_threads_pool_get(const struct cpumask *cpu_mask,
+int iscsi_threads_pool_get(const cpumask_t *cpu_mask,
 	struct iscsi_thread_pool **out_pool)
 {
 	int res;

iscsi-scst/kernel/iscsi.h
@@ -78,7 +78,7 @@ struct iscsi_thread_pool {
 	struct list_head wr_list;
 	wait_queue_head_t wr_waitQ;
 
-	struct cpumask cpu_mask;
+	cpumask_t cpu_mask;
 
 	int thread_pool_ref;
 
@@ -527,7 +527,7 @@ extern int iscsi_preliminary_complete(struct iscsi_cmnd *req,
 	struct iscsi_cmnd *orig_req, bool get_data);
 extern int set_scst_preliminary_status_rsp(struct iscsi_cmnd *req,
 	bool get_data, int key, int asc, int ascq);
-extern int iscsi_threads_pool_get(const struct cpumask *cpu_mask,
+extern int iscsi_threads_pool_get(const cpumask_t *cpu_mask,
 	struct iscsi_thread_pool **out_pool);
 extern void iscsi_threads_pool_put(struct iscsi_thread_pool *p);
 

scst/include/scst.h
@@ -26,6 +26,7 @@
 #include <linux/blkdev.h>
 #include <linux/interrupt.h>
 #include <linux/wait.h>
+#include <linux/cpumask.h>
 
 /* #define CONFIG_SCST_PROC */
 
@@ -57,6 +58,76 @@ typedef _Bool bool;
 #include <scst/scst_sgv.h>
 #else
 #include "scst_sgv.h"
+#endif
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 20)
+#define nr_cpu_ids NR_CPUS
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 28)
+#define cpumask_bits(maskp) ((maskp)->bits)
+#ifdef CONFIG_CPUMASK_OFFSTACK
+/* Assuming NR_CPUS is huge, a runtime limit is more efficient.  Also,
+ * not all bits may be allocated. */
+#define nr_cpumask_bits nr_cpu_ids
+#else
+#define nr_cpumask_bits NR_CPUS
+#endif
+
+/* verify cpu argument to cpumask_* operators */
+static inline unsigned int cpumask_check(unsigned int cpu)
+{
+#ifdef CONFIG_DEBUG_PER_CPU_MAPS
+	WARN_ON_ONCE(cpu >= nr_cpumask_bits);
+#endif /* CONFIG_DEBUG_PER_CPU_MAPS */
+	return cpu;
+}
+
+/**
+ * cpumask_next - get the next cpu in a cpumask
+ * @n: the cpu prior to the place to search (ie. return will be > @n)
+ * @srcp: the cpumask pointer
+ *
+ * Returns >= nr_cpu_ids if no further cpus set.
+ */
+static inline unsigned int cpumask_next(int n, const cpumask_t *srcp)
+{
+	/* -1 is a legal arg here. */
+	if (n != -1)
+		cpumask_check(n);
+	return find_next_bit(cpumask_bits(srcp), nr_cpumask_bits, n+1);
+}
+
+/**
+ * for_each_cpu - iterate over every cpu in a mask
+ * @cpu: the (optionally unsigned) integer iterator
+ * @mask: the cpumask pointer
+ *
+ * After the loop, cpu is >= nr_cpu_ids.
+ */
+#define for_each_cpu(cpu, mask)                         \
+	for ((cpu) = -1;                                \
+		(cpu) = cpumask_next((cpu), (mask)),    \
+		(cpu) < nr_cpu_ids;)
+
+/**
+ * cpumask_copy - *dstp = *srcp
+ * @dstp: the result
+ * @srcp: the input cpumask
+ */
+static inline void cpumask_copy(cpumask_t *dstp,
+				const cpumask_t *srcp)
+{
+	bitmap_copy(cpumask_bits(dstp), cpumask_bits(srcp), nr_cpumask_bits);
+}
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 26)
+static inline int set_cpus_allowed_ptr(struct task_struct *p,
+				       const cpumask_t *new_mask)
+{
+	return set_cpus_allowed(p, *new_mask);
+}
 #endif
 
 /*
@@ -2396,7 +2467,7 @@ struct scst_acg {
 	int acg_io_grouping_type;
 
 	/* CPU affinity for threads in this ACG */
-	struct cpumask acg_cpu_mask;
+	cpumask_t acg_cpu_mask;
 
 	unsigned int tgt_acg:1;
 
@@ -3895,7 +3966,7 @@ struct scst_sysfs_work_item {
 				bool is_tgt_kobj;
 				int io_grouping_type;
 				bool enable;
-				struct cpumask cpu_mask;
+				cpumask_t cpu_mask;
 			};
 		};
 		struct {

scst/src/scst_main.c
@@ -158,7 +158,7 @@ static int suspend_count;
 
 static int scst_virt_dev_last_id; /* protected by scst_mutex */
 
-struct cpumask default_cpu_mask;
+cpumask_t default_cpu_mask;
 
 static unsigned int scst_max_cmd_mem;
 unsigned int scst_max_dev_cmd_mem;

scst/src/scst_priv.h
@@ -193,7 +193,7 @@ extern spinlock_t scst_mgmt_lock;
 extern struct list_head scst_sess_init_list;
 extern struct list_head scst_sess_shut_list;
 
-extern struct cpumask default_cpu_mask;
+extern cpumask_t default_cpu_mask;
 
 struct scst_cmd_thread_t {
 	struct task_struct *cmd_thread;

scst/src/scst_sysfs.c
@@ -3176,8 +3176,13 @@ static ssize_t __scst_acg_cpu_mask_show(struct scst_acg *acg, char *buf)
 {
 	int res;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 28)
+	res = cpumask_scnprintf(buf, SCST_SYSFS_BLOCK_SIZE,
+		acg->acg_cpu_mask);
+#else
 	res = cpumask_scnprintf(buf, SCST_SYSFS_BLOCK_SIZE,
 		&acg->acg_cpu_mask);
+#endif
 	if (!cpus_equal(acg->acg_cpu_mask, default_cpu_mask))
 		res += sprintf(&buf[res], "\n%s\n", SCST_SYSFS_KEY_MARK);
 
@@ -3185,7 +3190,7 @@ static ssize_t __scst_acg_cpu_mask_show(struct scst_acg *acg, char *buf)
 }
 
 static int __scst_acg_process_cpu_mask_store(struct scst_tgt *tgt,
-	struct scst_acg *acg, struct cpumask *cpu_mask)
+	struct scst_acg *acg, cpumask_t *cpu_mask)
 {
 	int res = 0;
 	struct scst_session *sess;