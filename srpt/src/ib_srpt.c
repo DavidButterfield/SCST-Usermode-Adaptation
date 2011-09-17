DavidButterfield/SCST-Usermode-Adaptation

commit 4fc8b4213262c2b77cebdd57bc420a2b4305cf7c

ib_srpt: Make sure that rmmod finishes
git-svn-id: https://svn.code.sf.net/p/scst/svn/trunk@3858 d57e44dd-8a1f-0410-8b47-8ef2f437770f

srpt/src/ib_srpt.c
@@ -2154,6 +2154,7 @@ static bool __srpt_close_ch(struct srpt_rdma_ch *ch)
 		/* fall through */
 	case CH_LIVE:
 		was_live = true;
+		wake_up_process(&ch->thread);
 		if (ib_send_cm_dreq(ch->cm_id, NULL, 0) < 0)
 			PRINT_ERROR("sending CM DREQ failed.");
 		break;
@@ -3404,6 +3405,7 @@ static int srpt_release_sdev(struct srpt_device *sdev)
 				   ch->sess_name,
 				   ch->state,
 				   atomic_read(&ch->scst_sess->sess_cmd_count));
+			__srpt_close_ch(ch);
 		}
 		spin_unlock_irq(&sdev->spinlock);
 	}

commit 338ce57721582d4cabd6aa779759ad8f518b958b

Fix a typo introduced in r3858
git-svn-id: https://svn.code.sf.net/p/scst/svn/trunk@3859 d57e44dd-8a1f-0410-8b47-8ef2f437770f

srpt/src/ib_srpt.c
@@ -2154,7 +2154,7 @@ static bool __srpt_close_ch(struct srpt_rdma_ch *ch)
 		/* fall through */
 	case CH_LIVE:
 		was_live = true;
-		wake_up_process(&ch->thread);
+		wake_up_process(ch->thread);
 		if (ib_send_cm_dreq(ch->cm_id, NULL, 0) < 0)
 			PRINT_ERROR("sending CM DREQ failed.");
 		break;